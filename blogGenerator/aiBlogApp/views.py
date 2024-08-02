034df2f0137ef0f27d335a867d0da77317337074
munna710 <munna7777777.1.0@gmail.com>
2024-08-02 23:56:34 +0530

    chore: Add login, signup, and logout functionality and update URLs
    This commit adds the necessary views and templates for the login, signup, and logout functionality. It also updates the URLs to include these new paths. This will allow users to create an account, log in, and log out of the application.

--- blogGenerator/aiBlogApp/views.py
+++ blogGenerator/aiBlogApp/views.py
@@ -1,17 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request,'index.html')
 
+def generate_blog(request):
+    pass
+
 def user_login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('userName')
+        password = request.POST.get('password')
+        if not username or not password:
+            error_message = "Username and password are required"
+            return render(request, 'login.html', {'error_message': error_message})
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = "Username or password is incorrect"
+            return render(request, 'login.html', {'error_message': error_message})
+
+    return render(request, 'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('userName')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        repeatPassword = request.POST.get('repeatPassword')
+
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = "username already exists"
+                return render(request,'signup.html',{'error_message':error_message})
+
+        else:
+            error_message = "passwords do not match"
+            return render(request,'signup.html',{'error_message':error_message})
     return render(request,'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
     
 
  
