07b9083657dfc72d04cfc227070a602aca30d44e
Munna710 <munna7777777.1.0@gmail.com>
2024-08-03 17:25:59 +0530

    update views.py

--- blogGenerator/aiBlogApp/views.py
+++ blogGenerator/aiBlogApp/views.py
@@ -9,112 +9,144 @@ from pytube import YouTube
 from django.conf import settings
 import os
 import assemblyai as aai
-from transformers import pipeline
-import logging
-# Create your views here.
+from groq import Groq
+import subprocess
+from datetime import datetime
+from os import getenv
+from dotenv import load_dotenv
+from .models import BlogPost
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
+
+load_dotenv()
+
 @login_required
 def index(request):
     return render(request,'index.html')
 
 @csrf_exempt
 def generate_blog(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         try:
             data = json.loads(request.body)
             yt_link = data['link']
-            logging.debug(f"Received YouTube link: {yt_link}")
         except (KeyError, json.JSONDecodeError):
             return JsonResponse({'error': 'Invalid data sent'}, status=400)
 
+
         # get yt title
-        try:
-            title = yt_title(yt_link)
-            logging.debug(f"Retrieved YouTube title: {title}")
-        except Exception as e:
-            logging.error(f"Error retrieving YouTube title: {e}")
-            return JsonResponse({'error': 'Failed to retrieve YouTube title'}, status=500)
+        title = yt_title(yt_link)
 
         # get transcript
-        try:
-            transcription = get_transcription(yt_link)
-            logging.debug(f"Retrieved transcription: {transcription}")
-            if not transcription:
-                return JsonResponse({'error': 'Transcription failed'}, status=500)
-        except Exception as e:
-            logging.error(f"Error retrieving transcription: {e}")
-            return JsonResponse({'error': 'Failed to retrieve transcription'}, status=500)
+        transcription = get_transcription(yt_link)
+        if not transcription:
+            return JsonResponse({'error': " Failed to get transcript"}, status=500)
 
-        # generate blog
-        try:
-            blog_content = generate_blog_from_transcription(transcription)
-            logging.debug(f"Generated blog content: {blog_content}")
-            if not blog_content:
-                return JsonResponse({'error': 'Blog generation failed'}, status=500)
-        except Exception as e:
-            logging.error(f"Error generating blog content: {e}")
-            return JsonResponse({'error': 'Failed to generate blog content'}, status=500)
 
-        # save blog
-        try:
-            new_blog_article = BlogPost.objects.create(
-                user=request.user,
-                youtube_title=title,
-                youtube_link=yt_link,
-                generated_content=blog_content,
-            )
-            new_blog_article.save()
-            logging.debug(f"Saved new blog article: {new_blog_article.id}")
-        except Exception as e:
-            logging.error(f"Error saving blog article: {e}")
-            return JsonResponse({'error': 'Failed to save blog article'}, status=500)
+        # use OpenAI to generate the blog
+        blog_content = generate_blog_from_transcription(transcription)
+        if not blog_content:
+            return JsonResponse({'error': " Failed to generate blog article"}, status=500)
 
-        # return blog
+        # save blog article to database
+        new_blog_article = BlogPost.objects.create(
+            user=request.user,
+            youtube_title=title,
+            youtube_link=yt_link,
+            generated_content=blog_content,
+        )
+        new_blog_article.save()
+
+        # return blog article as a response
         return JsonResponse({'content': blog_content})
-
     else:
-        return JsonResponse({'error': 'Invalid Request'}, status=405)
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 def yt_title(link):
     yt = YouTube(link)
     title = yt.title
     return title
 
-def download_audio(link):
-    yt = YouTube(link)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path=settings.MEDIA_ROOT)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    return new_file
+# def download_audio(link):
+#     yt = YouTube(link)
+#     video = yt.streams.filter(only_audio=True).first()
+#     out_file = video.download(output_path=settings.MEDIA_ROOT)
+#     base, ext = os.path.splitext(out_file)
+#     new_file = base + '.mp3'
+#     os.rename(out_file, new_file)
+#     return new_file
 
+def download_audio_yt_dlp(link, output_path):
+    try:
+        os.makedirs(output_path, exist_ok=True)
+
+        # Use a timestamp to create a unique filename
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        output_template = os.path.join(output_path, f'%(title)s_{timestamp}.%(ext)s')
+
+        yt_dlp_cmd = [
+            'yt-dlp',
+            '-x', '--audio-format', 'mp3',
+            '-o', output_template,
+            link
+        ]
+        subprocess.run(yt_dlp_cmd, check=True)
+
+        # Find the newly downloaded file
+        for file in os.listdir(output_path):
+            if file.endswith('.mp3') and timestamp in file:
+                return os.path.join(output_path, file)
+
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+def download_audio(link):
+    return download_audio_yt_dlp(link, settings.MEDIA_ROOT)
+           
 def get_transcription(link):
     audio_file = download_audio(link)
-    logging.debug(f"Downloaded audio file: {audio_file}")
     
-    aai.settings.api_key = "your_assemblyai_api_key"
+    AAI_key = getenv('AAI')
+    aai.settings.api_key = AAI_key
+
     transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file)
     
-    try:
-        # Upload the audio file to AssemblyAI
-        upload_url = transcriber.upload(audio_file)
-        logging.debug(f"Uploaded audio file URL: {upload_url}")
-
-        # Transcribe the uploaded audio file
-        transcript = transcriber.transcribe(upload_url)
-        logging.debug(f"Transcription result: {transcript}")
-        return transcript.text
-    except Exception as e:
-        logging.error(f"Error during transcription: {e}")
-        raise
+    return transcript.text
 def generate_blog_from_transcription(transcription):
-    generator = pipeline('text-generation', model='gpt-2')
-    prompt = f"Generate a blog post based on the following transcription:\n\n{transcription}"
-    blog_content = generator(prompt, max_length=1024, num_return_sequences=1)[0]['generated_text']
-    return blog_content
+    prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a YouTube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
+    
+    client = Groq(api_key=getenv('GROQ_API_KEY'),)
+    completion = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
 
+    generated_content = "".join(chunk.choices[0].delta.content or "" for chunk in completion)
+
+    return generated_content
+
+
+def blog_list(request):
+    blog_articles = BlogPost.objects.filter(user=request.user)
+    return render(request, "all-blogs.html", {'blog_articles': blog_articles})
+
+def blog_details(request, pk):
+    blog_article_detail = BlogPost.objects.get(id=pk)
+    if request.user == blog_article_detail.user:
+        return render(request, 'blog-details.html', {'blog_article_detail': blog_article_detail})
+    else:
+        return redirect('/')   
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('userName')
@@ -131,6 +163,7 @@ def user_login(request):
             return render(request, 'login.html', {'error_message': error_message})
 
     return render(request, 'login.html')
+
 
 def user_signup(request):
     if request.method == 'POST':
